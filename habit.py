0ff2b6444e20e267c16f5a371b57be3dd02e57ff
liya-gafurova <gali4086555@gmail.com>
2021-08-11 09:16:15 +0600

    added skeleton of Habit class

---
+++ habit.py
@@ -0,0 +1,23 @@
+
+class Habit():
+    def __init__(self, name:str, description:str):
+        self.name = name
+        self.preconditions = []
+
+    @property
+    def place(self):
+        # at the working table
+        pass
+
+    @property
+    def time(self):
+        # 8.30 in the morning
+        pass
+
+    def set_schedule(self):
+        # everyday / choose days of the week ...
+        pass
+
+    def set_precondition(self):
+        # text description about what should be before habit
+        pass
