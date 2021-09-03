76326680bbf068ef7eb82a71c3cd6b1f6fbc7335
liya-gafurova <gali4086555@gmail.com>
2021-09-03 09:31:47 +0600

    finished repository - TODO - check concept

--- app/db/repository.py
+++ app/db/repository.py
@@ -1,7 +1,7 @@
-import uuid
+from peewee import fn
 
 from app.db.db import Habit, Preconditions, WeekPeriods
-from app.domain.habitentity import HabitEntity, HabitData
+from app.domain.habitentity import HabitEntity, HabitData, HabitSchedule, WeekPeriod
 
 
 class HabitRepository:
@@ -38,10 +38,6 @@ class HabitRepository:
         habit_obj: Habit = Habit.get_by_id(id)
         preconditions = Preconditions.select().where(Preconditions.habit == habit_obj)
 
-        days = WeekPeriods.select().where(WeekPeriods.habit == habit_obj)
-
-
-
         habit_entity = HabitEntity(habit_data=HabitData(
             name=habit_obj.name,
             description=habit_obj.description,
@@ -50,5 +46,30 @@ class HabitRepository:
 
         habit_entity.place = habit_obj.place
         # manage days
+        convert_days = lambda s: [int(i) for i in (s or '').split(',') if i]
+        one_period_days = (fn
+                           .GROUP_CONCAT(WeekPeriods.day)
+                           .python_value(convert_days))
+
+        query = (WeekPeriods
+                 .select(WeekPeriods.time, one_period_days.alias('days'), WeekPeriods.month, WeekPeriods.year)
+                 .where(WeekPeriods.habit == habit_obj)
+                 .group_by(WeekPeriods.time))
+
+        periods = []
+        month = ''
+        year = ''
+        for period in query:
+            print(period.days, period.time)
+            periods.append(
+                WeekPeriod(
+                    days_of_week=period.days,
+                    time=period.time
+                )
+            )
+            month, year = period.month, period.year
+
+        habit_entity.when(
+            habit_schedule=HabitSchedule(week_periods=periods, month=month, year=year))
 
         return habit_entity
